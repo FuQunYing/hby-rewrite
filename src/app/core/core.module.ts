2367ac09167832cd56b7d06abce37ea7619ed29d
FuQunYing <fqylovae@outlook.com>
2018-07-17 17:11:36 +0800

    core & service

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,3 +1,41 @@
 import { NgModule, Optional, SkipSelf} from '@angular/core';
 import { ServiceModule, SettingService} from '../service/service.module';
 import { HttpClientModule} from '@angular/common/http';
+
+@NgModule({
+  imports: [
+    HttpClientModule,
+    ServiceModule.forRoot()
+  ],
+  providers: [
+    {
+      provide: 'BASE_CONFIG',
+      useValue: {
+        uri: '/api'
+      }
+    }
+  ],
+  declarations: []
+})
+
+export class CoreModule {
+  constructor( @Optional() @SkipSelf() parentModule: CoreModule, private setting: SettingService) {
+    if (parentModule) {
+      throw new Error( 'CoreModule已经装载，请仅在AppModule中引入该模块');
+    }
+    let lang = localStorage.getItem('lang');
+    if (!lang) {
+      lang = navigator.language ? navigator.language : 'zh-cn';
+      localStorage.setItem('lang', lang);
+    }
+    if (lang.includes('zh')) {
+      if (lang.includes('TW') || lang.includes('HK')) {
+        this.setting.language = 'zh-Hans-HK';
+      } else {
+        this.setting.language = 'zh-Hans';
+      }
+    } else {
+      this.setting.language = 'en';
+    }
+  }
+}
--- src/app/service/setting.service.ts
+++ src/app/service/setting.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
 
-@Injectable({
-  providedIn: 'root'
-})
+export interface Layout {
+  fixed: boolean; // 是否固定顶部菜单
+  collapsed: boolean; // 是否折叠左侧菜单
+}
+@Injectable()
 export class SettingService {
+  public layout: Layout = {
+    collapsed: false,
+    fixed: true
+  };
 
-  constructor() { }
+  public language;
+
+  public userType: string = localStorage.getItem('user') ? JSON.parse(localStorage.getItem('user')).user_type : '';
+
+  setLayout(name: string, value: any) {
+    if (typeof this.layout[name] !== 'undefined') {
+      this.layout[name] = value;
+    }
+  }
 }
