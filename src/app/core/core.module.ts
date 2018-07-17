b17e514e959a707bd2815420f57a14f0517cd38f
FuQunYing <fqylovae@outlook.com>
2018-07-17 16:50:19 +0800

    first commit

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,3 @@
+import { NgModule, Optional, SkipSelf} from '@angular/core';
+import { ServiceModule, SettingService} from '../service/service.module';
+import { HttpClientModule} from '@angular/common/http';
--- src/app/service/service.module.ts
+++ src/app/service/service.module.ts
@@ -1,10 +1,60 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule, ModuleWithProviders } from '@angular/core';
+import {CookieService} from './cookie.service';
+import {AuthService} from './auth.service';
+import {AuthGuardService} from './auth-guard.service';
+import {MemberService} from './member.service';
+import {DeviceService} from './device.service';
+import {BankService} from './bank.service';
+import {SettingService} from './setting.service';
+import {GoodsService} from './goods.service';
+import {DashboardService} from './dashboard.service';
+import {SellService} from './sell.service';
+import {UserService} from './user.service';
+import {LoadingService} from './loading.service';
+import {LocationService} from './location.service';
+import {LocateDataService} from './locate-data.service';
+import {ActivityService} from './activity.service';
 
-@NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
-})
-export class ServiceModule { }
+export {
+  CookieService,
+  AuthService,
+  AuthGuardService,
+  MemberService,
+  DeviceService,
+  BankService,
+  SettingService,
+  GoodsService,
+  DashboardService,
+  SellService,
+  UserService,
+  LoadingService,
+  LocationService,
+  LocateDataService,
+  ActivityService
+};
+
+@NgModule()
+export class ServiceModule {
+  static forRoot(): ModuleWithProviders {
+    return {
+      ngModule: ServiceModule,
+      providers: [
+        CookieService,
+        AuthService,
+        AuthGuardService,
+        MemberService,
+        DeviceService,
+        BankService,
+        SettingService,
+        GoodsService,
+        DashboardService,
+        SellService,
+        UserService,
+        LoadingService,
+        LocationService,
+        LocateDataService,
+        ActivityService
+      ]
+    };
+  }
+}
