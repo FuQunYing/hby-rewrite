1f5ccbc3cfb43ddd5d18d6933c5c8ac6a3286eee
FuQunYing <fqylovae@outlook.com>
2018-07-20 11:12:33 +0800

    loadingService

--- src/app/service/loading.service.ts
+++ src/app/service/loading.service.ts
@@ -1,10 +1,36 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders} from '@angular/common/http';
+import { Observable} from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class GoodsService {
 
-  constructor() { }
+  private header = new HttpHeaders({
+    'x-access-token': localStorage.getItem('user') ? JSON.parse(localStorage.getItem('user')).token : '',
+    'secret': localStorage.getItem('user') ? JSON.parse(localStorage.getItem('user')).secret : '',
+    'Content-type': 'application/json; charset=UTF-8'
+  });
+  constructor(private http: HttpClient) { }
+
+  /**
+   * 商家用户获取所有loading页配置
+   * @returns {Observable<any>}
+   */
+  getLoadingCfg(): Observable<any> {
+    const uri = 'api/v1/shapi/ChannelApi/getLoadingCfg';
+    return this.http.post(uri, null, {headers: this.header});
+  }
+
+  /**
+   * 商家用户下修改loading页配置
+   * @param loading
+   * @returns {Observable<any>}
+   */
+  setLoadingCfg(loading): Observable<any> {
+    const uri = '/api/v1/shapi/ChannelApi/setLoadingCfg';
+    const body = { loading };
+    return this.http.post(uri, JSON.stringify(body), {headers: this.header});
+  }
 }
 
